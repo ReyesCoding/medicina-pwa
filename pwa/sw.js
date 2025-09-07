85521e71d5d959497565ed006884098c6f21d8cc
Bryan Reyes <reyes.codes@gmail.com>
2025-09-07 17:46:16 -0400

    update to v11

--- pwa/sw.js
+++ pwa/sw.js
@@ -1,5 +1,5 @@
 /* sw v1 */
-const CACHE = "medicina-static-v10";
+const CACHE = "medicina-static-v11";
 const ASSETS = [
   "./", "../index.html", "../styles.css",
   "../app.js", "../graph.js", "../planner.js", "../gpa.js", "../config.js",
